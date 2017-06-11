40cabd201d0e2278dc1bbce10fc342e187440229
Rimian Perkins <hello@rimian.com.au>
2017-06-11 13:36:26 +1000

    fix path to app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rspec'
 
 ENV['RACK_ENV'] = 'test'
 
-require File.expand_path '../../app.rb', __FILE__
+require File.expand_path '../../base.rb', __FILE__
 
 RSpec.configure do |c|
   c.include Rack::Test::Methods
