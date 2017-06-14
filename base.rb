7a65dc56708ecd575751edf193bc712547ab6e3a
Rimian Perkins <hello@rimian.com.au>
2017-06-14 15:14:55 +1000

    add initital submit form

--- base.rb
+++ base.rb
@@ -13,3 +13,42 @@ get '/' do
   @google_analytics_code = 'UA-100831510-1'
   erb :app
 end
+
+get '/success' do
+  'Yay!'
+end
+
+post '/' do
+  configure_pony
+  # name = params[:name]
+  # sender_email = params[:email]
+  # message = params[:message]
+  logger.error params.inspect
+  begin
+    Pony.mail(
+      :from => ENV['SENDGRID_RECIPIENT_ADDRESS'], # "#{name}<#{sender_email}>",
+      :to => ENV['SENDGRID_RECIPIENT_ADDRESS'], #'example@gmail.com',
+      :subject => 'test sendgrid', # "#{name} has contacted you",
+      :body => 'hello world', #"#{message}",
+    )
+    redirect '/success'
+  rescue
+    @exception = $!
+    erb :boom
+  end
+end
+
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true,
+      :domain               => 'heroku.com'
+    }
+  }
+end
